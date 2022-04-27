e180f0cf92bae39e6c9cc71f9cd2352deff7d4fa
Anedsii <angelacilez@gmail.com>
2022-04-27 00:36:43 -0300

    #change

--- PrimeraEntregaProyectoFinal/main.js
+++ PrimeraEntregaProyectoFinal/main.js
@@ -10,7 +10,42 @@
 6-modificar
 
 
-*/
+*/ 
+//Busqueda
+
+//ID
+function buscarid(id,datos){
+    let info = [];
+    for(i=0; i<datos.length; i++){
+        if(datos[i].id==id){
+            info.push(datos[i]);
+        }
+    }
+
+    return info
+}
+//nombre
+function buscarno(nombre,datos){
+    let infon = [];
+    for(i=0; i<datos.length; i++){
+        if(datos[i].componente==nombre){
+            infon.push(datos[i]);
+        }
+    }
+
+    return infon
+}
+//Funcicon Eliminar
+function eliminar(datos,id_ele){
+    let newdate=[]
+    for(i=0;i<datos.length;i++){
+        if(datos[i].id != id_ele){
+              newdate.push(datos[i]);
+        }
+    }
+
+    return newdate
+}
 // Creamo la estructura del objeto que vamos a utilizar de informacion por producto 
 
 class Producto{
@@ -19,32 +54,41 @@ class Producto{
         this.componente = componente;
         this.description = description;
         this.id = id;
-        this.pre_unitario = pre_unitario;
+        this.pre_unitario = pre_unitario * 1.21;
         this.datasheet = datasheet;
         this.estado = estado;
     }
-    iva(){
+  /*  iva(){
         this.pre_unitario = this.pre_unitario * 1.21;
-    }
-
-
+    }*/
 
 }
-let vector =[];
-const producto1 = new Producto("Anshu","hola",1,3,"http","stock");
-const producto2 = new Producto("Anshu","hola",1,4,"http","stock");
 
-vector.push(producto1);
-vector.push(producto2);
-console.log(producto1);
+// Variable que va contener la informacion de mis producto * Por el momento*
+let productos_data = [];
 
-producto1.iva(); //aplica la funcion 
-console.log(producto1);
-console.log(vector);
-console.log(vector[0].pre_unitario) //ver producto por fila
+//Ingreso de datos *Primeramente va ser de 4 //
+//Ya que el ingreso futuramente va ser con una base de datos //
 
-for (const ficher of vector) {
-    ficher.iva(); //aplica la funcion
-
+for(i=0; i<1; i++){
+    
+    productos_data.push(new Producto(prompt("Ingrese componente"),prompt("Ingrese description"),prompt("Ingrese ID"),prompt("Ingrese precio unitario"),prompt("Link Datashet"),prompt("Estado")));
 }
-console.log(vector);
+console.log(productos_data);
+
+//Eliminar
+
+
+productos_data= eliminar(productos_data,prompt("Ingrese ID del producto a eliminar"));
+console.log("Eliminado");
+console.log(productos_data);
+
+//Buscar id
+console.log("Buscar id");
+console.log(buscarid(prompt("ingrese id del producto a buscar"),productos_data));
+
+
+//Buscar por nombre
+
+console.log("Buscar nombre");
+console.log(buscarno(prompt("ingrese nombre del producto a buscar"),productos_data));
