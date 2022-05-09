477a2581894356f9d4619021f8dd8a8e01a36975
Anedsii <angelacilez@gmail.com>
2022-05-09 20:49:04 -0300

    #change

---
+++ Tercercomplementario/main.js
@@ -0,0 +1,12 @@
+function guardar(){
+   
+    let producto = document.getElementById("producto").value;
+    let Estado = document.getElementById("Estado").value;
+    let Proveedor = document.getElementById("Proveedor").value;
+
+    let fila="<tr><td>"+producto+"</td><td>"+Estado +"</td><td>"+Proveedor +"</td></tr>";
+
+    let btn = document.createElement("TR");
+   	btn.innerHTML=fila;
+    document.getElementById("tabla").appendChild(btn);
+}
